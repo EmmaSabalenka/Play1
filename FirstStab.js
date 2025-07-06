b971fce692b67a65746e270a3853d44214e2d007
EmmaSablenka <protonplc@proton.me>
2025-07-06 20:41:28 +0100

    Che che che che chaaaaaangeeeeessss

--- FirstStab.js
+++
@@ -1,7 +0,0 @@
-console.log("---------------------------------____");
-console.log("It's the wombles");
-console.log("of Wimbledon Common, we are the wombles");
-console.log("Wombles are organised, you see");
-console.log("Wombles are tidy, and Wombles are keen");
-console.log("Wombles are good at finding a way");
-console.log("---------------------------------____");
